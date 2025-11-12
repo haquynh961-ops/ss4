c49861cd934b88e98dd072aa40a512d7b5ee5023
haquynh961-ops <haquynh961@gmail.com>
2025-11-12 18:08:42 +0700

    Add files via upload

---
+++ ss4-cơ bản 4.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE companydb;
+use companydb;
+CREATE TABLE Employee(
+EmployeeID INT PRIMARY KEY AUTO_INCREMENT, 
+ FirstName VARCHAR ( 50) ,
+LastName VARCHAR ( 50 ) ,
+HireDate DATE ,
+Salary INT
+);
+UPDATE  Employee
+SET Salary = 4800 
+WHERE EmployeeID = 2 ;
+
+INSERT INTO Employees (FirstName, LastName, HireDate, Salary)
+VALUES
+('Nguyen', 'An', '2023-05-15', 12000000),
+('Tran', 'Binh', '2022-11-01', 15000000),
+('Le', 'Chi', '2024-02-20', 18000000);
+
+
