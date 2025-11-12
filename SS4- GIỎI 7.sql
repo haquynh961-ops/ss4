a815487d4b6a1202be5cbb5cc80f27c1471b1596
haquynh961-ops <haquynh961@gmail.com>
2025-11-12 21:45:38 +0700

    Add files via upload

---
+++ SS4- GIỎI 7.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE Companydb;
+use Companydb;
+CREATE TABLE Employees (
+EmployeeID INT PRIMARY KEY AUTO_INCREMENT ,
+FirstName VARCHAR ( 50 ) ,
+LastName VARCHAR ( 50) ,
+HireDate DATE , 
+Salary DECIMAL(10,2)
+ ) ;
+ SELECT * FROM Employees;
+SELECT * FROM Employees
+WHERE Salary > 5000;
+SELECT * FROM Employees
+ORDER BY HireDate DESC;
+SELECT Department, COUNT(*) AS SoLuongNhanVien
+FROM Employees
+GROUP BY Department;
+SELECT Department, AVG(Salary) AS LuongTrungBinh
+FROM Employees
+GROUP BY Department;
+SELECT Department, AVG(Salary) AS LuongTrungBinh
+FROM Employees
+GROUP BY Department
+HAVING AVG(Salary) > 5000;
+INSERT INTO Employees (FirstName, LastName, HireDate, Salary)
+VALUES
+('Nguyen', 'An', '2023-05-15', 12000000),
+('Tran', 'Binh', '2022-11-01', 15000000),
+('Le', 'Chi', '2024-02-20', 18000000);
+
+
