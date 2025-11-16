abb9864e02a795367a5ec642649d4804473358a1
haquynh961-ops <haquynh961@gmail.com>
2025-11-16 22:34:14 +0700

    Add files via upload

---
+++ ss6- giỏi 9.sql
@@ -0,0 +1,11 @@
+insert into PhieuMuon ( MaDocGia ,MaSach , NgayMuon,NgayTra) 
+VALUES 
+(1, 1, '2024-01-10', '2024-01-25'),
+(2, 3, '2024-02-15', NULL),
+(1, 2, '2024-03-01', '2024-03-15'),
+(3, 4, '2024-03-05', NULL),
+(5, 5, '2024-04-20', '2024-05-01');
+select * from PhieuMuon 
+where MaDocGia = 1;
+select * from PhieuMuon 
+where NgayTra is null;
